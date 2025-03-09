c8ccdcf94d73af7efdd949b42991959428e88417
Duarte Laia <duarte.laia@tecnico.ulisboa.pt>
2025-03-09 23:55:35 +0000

    feat: 3.1.8 (Service Test) Create Institution Profile Service Success (#1, close #62)

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/institutionprofile/service/CreateInstitutionProfileServiceTest.groovy
@@ -0,0 +1,54 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.beans.factory.annotation.Autowired
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.InstitutionProfileRepository
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.InstitutionProfileService
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.dto.InstitutionProfileDto
+import spock.lang.Unroll
+
+@DataJpaTest
+class CreateInstitutionProfileServiceTest extends SpockTest {
+    
+    @Autowired
+    InstitutionProfileService institutionProfileService
+
+    @Autowired
+    InstitutionProfileRepository institutionProfileRepository
+
+    def institution
+
+    def setup() {
+        institution = institutionService.getDemoInstitution()
+    }
+
+    def 'create institution profile' () {
+        given:
+        def institutionProfileDto = new InstitutionProfileDto()
+        institutionProfileDto.shortDescription = 'short description'
+
+        when:
+        def result = institutionProfileService.createInstitutionProfile(institution.id, institutionProfileDto)
+
+        then:
+        result.shortDescription == 'short description'
+        and:
+        institutionProfileRepository.findAll().size() == 1
+        def storedInstitutionProfile = institutionProfileRepository.findAll().get(0)
+        storedInstitutionProfile.shortDescription == 'short description'
+        storedInstitutionProfile.institution.id == institution.id
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {
+        @Bean
+        InstitutionProfileService institutionProfileService() {
+            return new InstitutionProfileService()
+        }
+    }
+}
