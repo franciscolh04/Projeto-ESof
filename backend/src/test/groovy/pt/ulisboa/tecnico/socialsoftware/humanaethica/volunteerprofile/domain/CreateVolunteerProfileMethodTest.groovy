3dc081bc242650ee79d0f5c48e5260e0d07dd9db
MAFALDA SZOLNOKY RAMOS PINTO DIAS <mafalda.p.dias@tecnico.ulisboa.pt>
2025-03-10 19:17:14 +0000

    feat: 2.1.8.a (Domain Test) Ensure Minimum Length for Volunteer Short Description Invariant - Success (#2, close #44)

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/volunteerprofile/domain/CreateVolunteerProfileMethodTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/volunteerprofile/domain/CreateVolunteerProfileMethodTest.groovy
@@ -35,6 +35,23 @@ class CreateVolunteerProfileMethodTest extends SpockTest {
         "      Trim      "       || ErrorMessage.SHORT_BIO_TOO_SHORT
     }
 
+    @Unroll
+    def "create volunteer profile with valid shortBio: #shortBio"() {
+        given: "a valid shortBio"
+        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
+        volunteerProfileDto.setShortBio(shortBio)
+
+        when:
+        VolunteerProfile volunteerProfile = new VolunteerProfile(volunteerProfileDto, volunteer)
+
+        then: "no exception should be thrown and the shortBio should be set correctly"
+        volunteerProfile.getShortBio() == shortBio
+
+        where:
+        shortBio << ["This is a valid short bio", "Sebastiana", "Valid                  bio", "B        i" ]
+
+    }
+
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
 }
