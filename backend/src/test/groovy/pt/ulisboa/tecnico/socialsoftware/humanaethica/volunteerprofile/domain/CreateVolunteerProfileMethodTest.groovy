471d74221a18542757ccf916b3ca857c731338a4
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2025-03-11 15:41:35 +0000

    feat: 2.1.7 (Domain Test) VolunteerProfile Creation - Success (#2, close #42)

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/volunteerprofile/domain/CreateVolunteerProfileMethodTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/volunteerprofile/domain/CreateVolunteerProfileMethodTest.groovy
@@ -14,14 +14,62 @@ import spock.lang.Unroll
 @DataJpaTest
 class CreateVolunteerProfileMethodTest extends SpockTest {
     Volunteer volunteer = Mock()
-    Participation participation1 = Mock()
-    Participation participation2 = Mock()
-    Participation participation3 = Mock()
+    Participation part1 = Mock()
+    Participation part2 = Mock()
+    Participation part3 = Mock()
+    def volunteerProfileDto
+
+    def setup() {
+        given: "volunteerProfileDto"
+        volunteerProfileDto = new VolunteerProfileDto()
+    }
+
+    def createList(Participation part1, Participation part2, Participation part3,
+                   boolean includePart1, boolean includePart2, boolean includePart3) {
+        def list = []
+        if (includePart1) list << part1
+        if (includePart2) list << part2
+        if (includePart3) list << part3
+        return list
+    }
+
+    @Unroll
+    def "create a valid volunteer profile: rating1=#rating1 | rating2=#rating2 | rating3=#rating3 | avgRating=#avgRating | includePart1=#includePart1 | includePart2=#includePart2 | includePart3=#includePart3 | totalEnr=#totalEnr | totalPart=#totalPart | totalAss=#totalAss | shortBio=#shortBio"() {
+        given: "a valid volunteer"
+        part1.getMemberRating() >> rating1
+        part2.getMemberRating() >> rating2
+        part3.getMemberRating() >> rating3
+        def list = createList(part1, part2, part3, includePart1, includePart2, includePart3)
+        volunteerProfileDto.setShortBio(shortBio)
+        volunteerProfileDto.setNumTotalEnrollments(totalEnr)
+        volunteerProfileDto.setNumTotalParticipations(totalPart)
+        volunteerProfileDto.setNumTotalAssessments(totalAss)
+        volunteerProfileDto.setAverageRating(avgRating)
+        volunteerProfileDto.setSelectedParticipations(list)
+
+        when:
+        def result = new VolunteerProfile(volunteer, volunteerProfileDto)
+
+        then: "check result"
+        result.getShortBio() == shortBio
+        result.getNumTotalEnrollments() == totalEnr
+        result.getNumTotalParticipations() == totalPart
+        result.getNumTotalAssessments() == totalAss
+        result.getAverageRating() == avgRating
+        result.getSelectedParticipations() == list
+
+
+        where:
+        rating1 | rating2 | rating3 | avgRating | includePart1 | includePart2 | includePart3 | totalEnr | totalPart | totalAss | shortBio
+        1       | 1       | 1       | 1         | true         | true         | true         | 7        | 6         | 4        | "Valid        bio"
+        3       | 4       | null    | 3.5       | true         | true         | false        | 4        | 4         | 2        | "B            i"
+        null    | null    | null    | null      | false        | false        | false        | 0        | 0         | 0        | VOLUNTEER_PROFILE_SHORT_BIO_VALID
+
+    }
 
     @Unroll
     def "create volunteer profile and violate shortBio minimum length invariant: #shortBio"() {
         given: "an invalid shortBio"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
         volunteerProfileDto.setShortBio(shortBio)
 
         when:
@@ -40,30 +88,12 @@ class CreateVolunteerProfileMethodTest extends SpockTest {
     }
 
     @Unroll
-    def "create volunteer profile with valid shortBio: #shortBio"() {
-        given: "a valid shortBio"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
-        volunteerProfileDto.setShortBio(shortBio)
-
-        when:
-        VolunteerProfile volunteerProfile = new VolunteerProfile(volunteer, volunteerProfileDto)
-
-        then: "no exception should be thrown and the shortBio should be set correctly"
-        volunteerProfile.getShortBio() == shortBio
-
-        where:
-        shortBio << [VOLUNTEER_PROFILE_SHORT_BIO_VALID, "Sebastiana", "Valid                  bio", "B        i" ]
-
-    }
-
-    @Unroll
     def "create volunteer profile and violate selected participations assessed invariant: memberRating1=#memberRating1 | memberRating2=#memberRating2 | memberRating3=#memberRating3"() {
         given: "a volunteer profile with selected participations not assessed"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
-        participation1.getMemberRating() >> memberRating1
-        participation2.getMemberRating() >> memberRating2
-        participation3.getMemberRating() >> memberRating3
-        volunteerProfileDto.setSelectedParticipations([participation1, participation2, participation3])
+        part1.getMemberRating() >> memberRating1
+        part2.getMemberRating() >> memberRating2
+        part3.getMemberRating() >> memberRating3
+        volunteerProfileDto.setSelectedParticipations([part1, part2, part3])
         volunteerProfileDto.setShortBio(VOLUNTEER_PROFILE_SHORT_BIO_VALID)
 
         when:
@@ -82,34 +112,11 @@ class CreateVolunteerProfileMethodTest extends SpockTest {
     }
 
     @Unroll
-    def "create volunteer profile with selected participations assessed: memberRating1=#memberRating1 | memberRating2=#memberRating2 | memberRating3=#memberRating3"() {
-        given: "a valid volunteer profile with selected participations assessed"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
-        participation1.getMemberRating() >> memberRating1
-        participation2.getMemberRating() >> memberRating2
-        participation3.getMemberRating() >> memberRating3
-        volunteerProfileDto.setSelectedParticipations([participation1, participation2, participation3])
-        volunteerProfileDto.setShortBio(VOLUNTEER_PROFILE_SHORT_BIO_VALID)
-
-        when:
-        VolunteerProfile volunteerProfile = new VolunteerProfile(volunteer, volunteerProfileDto)
-
-        then: "no exception should be thrown"
-        volunteerProfile.getSelectedParticipations() == [participation1, participation2, participation3]
-
-        where:
-        memberRating1  | memberRating2  | memberRating3
-        1              | 1              | 1
-        3              | 4              | 5
-    }
-
-    @Unroll
     def "create volunteer profile and violate the minimum selected participations invariant: totalParticipations=#totalParticipations | totalAssessments=#totalAssessments"() {
         given: "a volunteer profile with an invalid number of selected participations"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
-        participation1.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
-        participation2.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
-        volunteerProfileDto.setSelectedParticipations([participation1, participation2])
+        part1.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
+        part2.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
+        volunteerProfileDto.setSelectedParticipations([part1, part2])
         volunteerProfileDto.setNumTotalParticipations(totalParticipations)
         volunteerProfileDto.setNumTotalAssessments(totalAssessments)
         volunteerProfileDto.setShortBio(VOLUNTEER_PROFILE_SHORT_BIO_VALID)
@@ -128,32 +135,6 @@ class CreateVolunteerProfileMethodTest extends SpockTest {
         6                    | 7                || ErrorMessage.SELECTED_PARTICIPATIONS_INVALID_NUMBER
         8                    | 3                || ErrorMessage.SELECTED_PARTICIPATIONS_INVALID_NUMBER
         6                    | 3                || ErrorMessage.SELECTED_PARTICIPATIONS_INVALID_NUMBER
-    }
-
-    @Unroll
-    def "create volunteer profile with minimum selected participations: totalParticipations=#totalParticipations | totalAssessments=#totalAssessments"() {
-        given: "a valid volunteer profile with a valid number of selected participations"
-        VolunteerProfileDto volunteerProfileDto = new VolunteerProfileDto()
-        participation1.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
-        participation2.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
-        participation3.getMemberRating() >> VALID_PARTICIPATION_MEMBER_RATING
-        volunteerProfileDto.setSelectedParticipations([participation1, participation2, participation3])
-        volunteerProfileDto.setNumTotalParticipations(totalParticipations)
-        volunteerProfileDto.setNumTotalAssessments(totalAssessments)
-        volunteerProfileDto.setShortBio(VOLUNTEER_PROFILE_SHORT_BIO_VALID)
-
-
-        when:
-        VolunteerProfile volunteerProfile = new VolunteerProfile(volunteer, volunteerProfileDto)
-
-        then: "no exception should be thrown and the number of selected participations should be valid"
-        volunteerProfile.getSelectedParticipations().size() == 3
-
-        where:
-        totalParticipations  | totalAssessments
-        6                    | 7
-        8                    | 3
-        6                    | 3
     }
 
     @TestConfiguration
