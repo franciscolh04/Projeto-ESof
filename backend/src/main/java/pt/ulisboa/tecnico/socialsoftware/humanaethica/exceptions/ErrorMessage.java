9f07d37d02725dd03588942af6f82a2eef85d771
Eduardo Silva <eduardomsilva@tecnico.ulisboa.pt>
2025-03-11 20:38:13 +0000

    feat: 3.2.1 (Invariant) Implement Most Recent 20% Selected Evaluations (#4, close #8)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/exceptions/ErrorMessage.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/exceptions/ErrorMessage.java
@@ -73,7 +73,7 @@ public enum ErrorMessage {
     INSTITUTION_PROFILE_DESCRIPTION_TOO_SHORT("Institution profile description is too short (must have at least 10 characters)"),
     MEMBER_NOT_IN_INSTITUTION("Member is not in the institution"),
     INSTITUTION_SELECTED_ASSESSMENTS("At least 50% of the assessments must be selected"),
-
+    INSTITUTION_MOST_RECENT_ASSESSMENTS("At least 20% of the selected assessments must be the most recent ones")
 
     ;
 
