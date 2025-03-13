34ceed05c25db289085eadf1d66a35a379773b50
Duarte Laia <duarte.laia@tecnico.ulisboa.pt>
2025-03-13 00:01:37 +0000

    feat: 3.2.8 (Service Test) Institution Profile Search Service Input Verification / Checking (#4, close #66)

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/institutionprofile/service/GetInstitutionProfileServiceTest.groovy
@@ -0,0 +1,48 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionProfile.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.InstitutionProfileRepository
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.InstitutionProfileService
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institutionprofile.dto.InstitutionProfileDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
+
+@DataJpaTest
+class GetInstitutionProfileServiceTest extends SpockTest {
+    def institution
+
+    def setup() {
+        institution = institutionService.getDemoInstitution()
+    }
+
+
+    def "invalid arguments: institution=#institutionid"() {
+        when:
+        institutionProfileService.getInstitutionProfile(institutionid)
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == errorMessage
+
+        where:
+        institutionid   || errorMessage
+        null            || ErrorMessage.INSTITUTION_NOT_FOUND
+        222             || ErrorMessage.INSTITUTION_NOT_FOUND
+    }
+
+    def "invalid arguments: institution profile"() {
+        when:
+        institutionProfileService.getInstitutionProfile(institution.id)
+        
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == ErrorMessage.INSTITUTION_PROFILE_NOT_FOUND
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
