fa66dc4354a57f48e1ac2124b76e0cf4fe1deb78
Diogo Almada <diogo.almada@tecnico.ulisboa.pt>
2025-03-13 14:21:10 +0000

    feat: 1.2.2 Implement controller for getSuggestedActivities: Only a member of the institution may obtain the list of suggested activities for that institution(#18, close #26)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activitysuggestion/ActivitySuggestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activitysuggestion/ActivitySuggestionController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activitysuggestion.dto.ActivitySuggestionDto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import java.util.List;
 
 import java.security.Principal;
 
@@ -24,5 +25,11 @@ public class ActivitySuggestionController {
                                                           @Valid @RequestBody ActivitySuggestionDto activitySuggestionDto) {
         int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
         return activitySuggestionService.createActivitySuggestion(userId, institutionId, activitySuggestionDto);
+    }
+
+    @GetMapping("/{institutionId}")
+    @PreAuthorize("hasRole('ROLE_MEMBER') and hasPermission(#institutionId, 'INSTITUTION.MEMBER')")
+    public List<ActivitySuggestionDto> getActivitySuggestionsByInstitution(@PathVariable Integer institutionId) {
+        return activitySuggestionService.getActivitySuggestionsByInstitution(institutionId);
     }
 }
