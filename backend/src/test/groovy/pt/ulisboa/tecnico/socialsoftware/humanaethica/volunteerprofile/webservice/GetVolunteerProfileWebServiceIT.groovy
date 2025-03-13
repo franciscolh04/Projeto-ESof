8eced479f327f578522ca2c8a0c463c32cbe7315
MAFALDA SZOLNOKY RAMOS PINTO DIAS <mafalda.p.dias@tecnico.ulisboa.pt>
2025-03-13 19:15:27 +0000

    feat: 2.2.3.a (Web Service Test) Get VolunteerProfile - Success (#32, close #54)

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/volunteerprofile/webservice/GetVolunteerProfileWebServiceIT.groovy
@@ -0,0 +1,55 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.volunteerprofile.webservice
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.http.HttpStatus
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.volunteerprofile.domain.VolunteerProfile
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.volunteerprofile.dto.VolunteerProfileDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetVolunteerProfileWebServiceIT extends SpockTest {
+
+    @LocalServerPort
+    private int port
+
+    def volunteer
+    def volunteerProfile
+
+    def setup() {
+        deleteAll()
+
+        webClient = WebClient.create("http://localhost:" + port)
+        headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_JSON)
+
+        volunteer = new Volunteer()
+        userRepository.save(volunteer)
+        volunteerProfile = new VolunteerProfile()
+        volunteerProfile.volunteer = volunteer
+        volunteer.volunteerProfile = volunteerProfile
+        volunteerProfileRepository.save(volunteerProfile)
+    }
+
+    def 'get volunteerProfile as a volunteer' () {
+        given:
+        def authVolunteer = demoVolunteerLogin()
+
+        when:
+        def response = webClient.get()
+                .uri('/volunteers/' + volunteer.id + '/profile')
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .retrieve()
+                .bodyToMono(VolunteerProfileDto.class)
+                .block()
+
+        then: "the volunteer profile is returned"
+        response.id == volunteerProfile.id
+    }
+
+}
